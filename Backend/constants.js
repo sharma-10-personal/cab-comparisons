82b207f7fd25ce73056d08b884b993aca8ae7726
unknown <2000sharma.ss@gmail.com>
2024-06-17 20:47:42 +0530

    constants

--- Backend/constants.js
+++ Backend/constants.js
@@ -1,5 +1,17 @@
 require('dotenv').config()
 
 module.exports ={
-    google_maps_api_key : process.env.GOOGLE_API_KEY || 'xyx'
+    google_maps_api_key : process.env.GOOGLE_API_KEY || 'xyx',
+    uber_equivalents : {
+    "UberXL": "Suv",
+    "Premier": "Sedan",
+    "Go Sedan": "Luxurious Sedan",
+    "UberXS": "Hatchback"
+    },
+    ola_equivalents : {
+        "prime" : "Luxurious Sedan",
+        "mini" : "Hatchback",
+        "suv"  : "Suv",
+        "auto" : "Auto"
+    }
 }
