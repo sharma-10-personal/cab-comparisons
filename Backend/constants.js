9f9810106a9f3182bb9cb3f93ff3f1e5daefcd5e
unknown <2000sharma.ss@gmail.com>
2024-07-06 18:35:58 +0530

    code restructuring

--- Backend/constants.js
+++ Backend/constants.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 
 module.exports ={
-    google_maps_api_key : process.env.GOOGLE_API_KEY || 'xyx',
+    google_maps_api_key : process.env.GOOGLE_API_KEY,
+    uber_cookie_key : process.env.UBER_KEY,
+    ola_cookie_key : process.env.OLA_KEY,
     uber_equivalents : {
     "UberXL": "Suv",
     "Premier": "Sedan",
--- Backend/ola.js
+++ Backend/ola.js
@@ -1,5 +1,6 @@
 const { default: axios } = require("axios");
-const { ola_equivalents } = require("./constants");
+const { ola_equivalents, ola_cookie_key } = require("./constants");
+const constants = require("./constants");
 
 async function getOlaPrice (ride_coordinates) {
 
@@ -16,9 +17,8 @@ async function getOlaPrice (ride_coordinates) {
     'accept': 'application/json', 
     'accept-language': 'en-US,en;q=0.9', 
     'content-type': 'application/json', 
-    'cookie': '_csrf=v6l5Ntn4FIGYOYQHDc9xtW-T; XSRF-TOKEN=bIWiwrJz-VIOAZ5K1ZHKOZ9CXE54O_0J6nZ; OSRN_v1=zXuBLcqCbFsSl6_YnQ3pUmiI; AKA_A2=A; _gid=GA1.2.610457421.1717316910; _gcl_au=1.1.1345583378.1717316912; _ga_EKVXJMSBW2=GS1.2.1717316928.1.1.1717317110.60.0.0; wasc=web-2d29a65a-53f5-4471-9b6c-0b5b1b75d6da__AQECAHgfxP3kLfatAqX5D3Wm8Q4cwpCiqFMlbQIth8I9m4HyQQAAANswgdgGCSqGSIb3DQEHBqCByjCBxwIBADCBwQYJKoZIhvcNAQcBMB4GCWCGSAFlAwQBLjARBAxWWFyQDm13OmD%2FonoCARCAgZNV3F2uqXnJMmQrCA24Hyv7KCGfwPvnRM%2FcUiXLEd9bwWjs%2FrVElR2iQj6vsFcda2v7hMMiiLQ2JF94rQLTmttNWd9Ekfer7z%2Fh2QrR1yb0s5ayMBqOzw4CP5E7B8DkMMKnGpaTKyg47%2B%2BsC2qvPEa04y2D4VIG8MX4vvvEDvWOZuSw7OwG9lAtNUVNXieIgJvIBgU%3D; _ga=GA1.1.220158383.1717316910; _ga_FR59878HTR=GS1.2.1717316911.1.1.1717318317.60.0.0; _ga_2TR8WHTK1X=GS1.1.1717316911.1.1.1717318760.49.0.0; OSRN_v1=BT0s6c7bfyEND82Kghzd3P96; wasc=web-52540907-e5ae-4422-9e41-d85435ecf9ba__AQECAHgfxP3kLfatAqX5D3Wm8Q4cwpCiqFMlbQIth8I9m4HyQQAAANswgdgGCSqGSIb3DQEHBqCByjCBxwIBADCBwQYJKoZIhvcNAQcBMB4GCWCGSAFlAwQBLjARBAxznHe0B%2BnOcFqZJ9ICARCAgZNgVTMAisRvIRap4DZCa8Mi2kVyCUaMVNk4eVqmyNNj5HJnR8Z1azyF01HkCYeWDAyFoyToEnEejButOygaZ55W1JOkRxJzY9CPV0NT4i3Sw08bxnc7gZeGP3sjT0Ae7%2B4uNHrQ1HDlZppJGdeWPtX9Jra4%2BUiud1%2FKqVFJFEROMTZyJh3DN%2B9i9FCDxcWjywHwy1s%3D'
-  }
-};
+    'cookie': constants.ola_cookie_key,
+  }};
 try {
 let final_result = await axios(config);
 console.log(final_result.data);
